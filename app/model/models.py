c5faedd64ea8f5ac69deece93d7c31b666cf222d
Jairo Matos da Rocha <devjairomr@gmail.com>
2022-11-04 14:13:24 -0300

    Payload

--- app/model/models.py
+++ app/model/models.py
@@ -7,3 +7,45 @@ from pymongo import MongoClient
 from app.config import settings, logger
 from app.db import MongoModel, PyObjectId
 
+from pydantic import BaseModel
+
+class Region(BaseModel):
+    text: str
+    value: str
+    type: str
+    
+class Filter(BaseModel):
+    valueFilter: str
+    viewValueFilter: str
+
+class Download(BaseModel):
+    csv: bool
+    shp: bool
+    gpkg: bool
+    raster: bool
+    layerTypeName: str
+    loading: bool
+    
+class Layer(BaseModel):
+    valueType: str
+    type: str
+    origin: Dict
+    typeLayer: str
+    viewValueType: str
+    typeLabel: str
+    wfsMapCard: Dict
+    download: Download
+    regionFilter: bool
+    filters: List[Filter]
+    filterLabel: str
+    filterSelected: str
+    filterHandler: str
+    visible: bool
+    opacity: float
+    metadata: List[Dict]
+    
+class Payload(BaseModel):
+    layer: Layer
+    region: str
+    filter: str
+    typeDownload: str
