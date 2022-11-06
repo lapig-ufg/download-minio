2606b146fd7ac7261f4c9b52e4b391bb27cb452c
FJairo Matos Da Rocha <devjairomr@gmail.com>
2022-11-05 21:41:22 -0300

    url

--- app/api/download.py
+++ app/api/download.py
@@ -3,10 +3,27 @@ from fastapi import APIRouter, HTTPException
 from app.model.payload import Payload
 from app.config import settings, logger
 from minio import Minio
+import pandas as pd
+import geopans as gpd
+
+from pydantic import BaseModel, HttpUrl
 
 router = APIRouter()
 
-@router.post('/', response_description="Dowload", response_model=Dict)
+class DowloadUrl(BaseModel):
+    object_name: str
+    size: int
+    host: str = settings.MINIO_HOST
+    buckt:str = settings.BUCKET
+    url: HttpUrl = ''
+    def __init__(self,**data) -> None:
+        super().__init__(**data)
+        self.url = f"https://{self.host}/{self.buckt}/{self.object_name}"
+        
+        
+
+
+@router.post('/', response_description="Dowload",  response_model=DowloadUrl)
 async def start_dowload(payload: Payload):
     
     query = {
@@ -19,21 +36,29 @@ async def start_dowload(payload: Payload):
         layerName = payload.layer.filterSelected
     else:
         layerName = payload.layer.valueType
-        
-    if payload.filter is None: 
+    
+    logger.debug(payload.filter)
+    try:
         fileParam = f"{payload.layer.valueType}_{payload.filter.valueFilter}"
-    else:
+    except AttributeError:
         fileParam = payload.layer.valueType
+    except Exception as e:
+        logger.exception('erro')
+        return HTTPException(500,f'e')
     
 
     pathFile = f"{payload.region.type}/{payload.region.value}/{payload.typeDownload}/{payload.layer.valueType}/{fileParam}"
     logger.info(f'{pathFile}')
     client = Minio(settings.MINIO_HOST, settings.MINIO_USER, settings.MINIO_PASSWORD, secure=True)
     objects = client.list_objects(
-            "ows", prefix=pathFile, recursive=True,
+            settings.BUCKET, prefix=f"{pathFile}.zip", recursive=True,
         )
-    if len(list(objects)) == 1:
-        return f"{settings.MINIO_HOST}/ows/{pathFile}"
+    objects_list = list(objects)
+    if len(objects_list) == 1:
+        return DowloadUrl(
+            object_name = objects_list[0].object_name,
+            size = objects_list[0].size
+            )
         
     return dict(payload)
 
