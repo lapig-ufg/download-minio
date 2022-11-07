f6d8385b41b4c1a3ddecdd32efb670a723acffd3
FJairo Matos Da Rocha <devjairomr@gmail.com>
2022-11-06 22:01:23 -0300

    dev

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -3,4 +3,4 @@
 source .env
 ssh -p 2522 -fN root@$SERVER -L 5432:127.0.0.1:5432
 cd /APP/pgrass-server
-gunicorn -k  uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 -w 4 -t 0 app.server:app
+gunicorn -k  uvicorn.workers.UvicornWorker --bind 0.0.0.0:8282 -w 4 -t 0 app.server:app
